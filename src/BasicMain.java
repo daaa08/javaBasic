5a7112df4d7d32f877dc8bc8cdd4beb88d5f65d8
박다정 <Da08@bagdajeong-ui-MacBook-Pro.local>
2017-05-10 21:20:20 +0900

    third push

---
+++ src/BasicMain.java
@@ -0,0 +1,68 @@
+package javaBasic;
+
+public class BasicMain {
+
+
+	public static void main(String[] args) {
+
+		// start부터 end값을 더한 값
+		long start = 0;
+		long end = 9999999999l;
+
+//		double result = 0;
+
+//		for(long i = start ; i<= end; i=i+1){
+//
+//			result = result +i;
+		System.out.print(end * end / 2);
+		}
+
+
+
+//	int a = 72;
+//	int b = 12;
+	//반복문
+//	//for(초기화 값; 종료값 ; 증감)
+////	for (int i=0; i<100; i=i+1){   //i++ = i=i+1
+////		System.out.print(i);
+////	}
+//
+//	// 1부터 100까지 더한 결과
+//	// 1. 결과값을 저장한 변수를 선언
+//	int result =0;
+//
+//	// 2. 로직을 수정
+//	for(int i=0; i<= 100; i=i+1){
+//		if(i%2 ==0){   // 짝수값만 더했을 때
+//		result = result + i;
+//		}
+//	}
+//	// 3. 결과값을 출력
+//	System.out.print("결과값" + result);
+
+
+
+
+	//조건문 if
+//		if(a>b){   //조건은 참/거짓이되어야
+//			System.out.print("a가 b보다 큽니다");
+//		}else if(a == b){
+//			System.out.print("a랑 b가 같습니다");
+//
+//		}else {
+//			System.out.print("a가 b 보다 작습니다");
+//		}
+
+
+
+	// 조건문 switch
+//		switch(a){
+//			case 72:
+//				System.out.print("a의 값은 37입니다.");
+//				break;
+//			case 12:
+//				System.out.print("a의 값은 38입니다.");
+//		}
+//	}
+
+}
