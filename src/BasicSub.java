16922660d5650fe7be55269f47fc2e1e0bb53695
박다정 <Da08@bagdajeong-ui-MacBook-Pro.local>
2017-05-10 21:27:36 +0900

    sub

---
+++ src/BasicSub.java
@@ -0,0 +1,65 @@
+package javaBasic;
+
+public class BasicSub {
+	public static void main(String[] args) {
+
+		long start = 0;
+		long end = 9999999999l;
+
+		//public void compare(){
+
+			int a = 2;
+			int b = 3;
+			int c = 5;
+
+//			if(a>b && a<c){  // 중요한걸 앞에
+//				System.out.print("a는 b와 c보다 작습니다.");
+
+			if( a > 0 ){
+				System.out.print(true);
+			}else{
+				System.out.print(false);
+			}
+				System.out.print(a > b ? true : false);  // 3항 연산
+			}
+
+
+
+
+//	public void casting(){
+//
+//		int number = 555;
+//
+//		String num = "1000.35";   // 숫자 안에 문자를 넣어 변형할 경우 에러가 뜸.
+//
+//		Double dbl = Double.parseDouble(num); // 소수점이 포함된 문자를 숫자를 변형
+//
+//		int num_temp = Integer.parseInt(num); //문자열을 숫자로 변형
+//
+//		//숫자를 문자로 바꾸기
+//		String number_temp = number + "";
+//
+//		System.out.print(number + num);
+//	}
+//
+//	public void sumFloat(float a, float b){
+//
+//		Float temp_a = a;
+//		Float temp_b = b;
+//
+//		float result = Float.sum(a, b);
+
+//	int a = 1;
+//	int b = 2;
+//
+//	System.out.println(a==b);
+//	System.out.println(a>b);
+//	System.out.println(!(a>b));
+//	System.out.println(b==2);
+//	System.out.println(!(a!=b));
+//	System.out.println();
+//
+//	System.out.println(true == false);
+//	System.out.println(true == (1>0));
+//	System.out.println(!!!!true);
+	}
